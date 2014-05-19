ec598e697a6dd53f6c3d1748c1a441abcaf82382
chocoby <chocoby@gmail.com>
2014-05-20 00:22:37 +0900

    WXViewController 作成

---
+++ SimpleWeather/SimpleWeather/WXViewController.h
@@ -0,0 +1,13 @@
+//
+//  WXViewController.h
+//  SimpleWeather
+//
+//  Created by chocoby on 5/20/14.
+//  Copyright (c) 2014 chocoby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXViewController : UIViewController
+
+@end
---
+++ SimpleWeather/SimpleWeather/WXViewController.m
@@ -0,0 +1,49 @@
+//
+//  WXViewController.m
+//  SimpleWeather
+//
+//  Created by chocoby on 5/20/14.
+//  Copyright (c) 2014 chocoby. All rights reserved.
+//
+
+#import "WXViewController.h"
+
+@interface WXViewController ()
+
+@end
+
+@implementation WXViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
