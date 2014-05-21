b4f394394dd564eb96a7897480e82960a3af6b0a
chocoby <chocoby@gmail.com>
2014-05-21 10:48:30 +0900

    Map JSON values to Objective-C properties.

--- SimpleWeather/SimpleWeather/WXCondition.m
+++ SimpleWeather/SimpleWeather/WXCondition.m
@@ -46,4 +46,23 @@
     return [WXCondition imageMap][self.icon];
 }
 
++ (NSDictionary *)JSONKeyPathsByPropertyKey
+{
+    return @{
+             @"date": @"dt",
+             @"locationName": @"name",
+             @"humidity": @"main.humidity",
+             @"temperature": @"main.temp",
+             @"tempHigh": @"main.temp_max",
+             @"tempLow": @"main.temp_min",
+             @"sunrise": @"sys.sunrise",
+             @"sunset": @"sys.sunset",
+             @"conditionDescription": @"weather.description",
+             @"condition": @"weather.main",
+             @"icon": @"weather.icon",
+             @"windBearing": @"wind.deg",
+             @"windSpeed": @"wind.speed"
+             };
+}
+
 @end
