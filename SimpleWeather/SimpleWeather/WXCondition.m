6760c35b3e66a8e87f40c8d95ae9403ac9691a25
chocoby <chocoby@gmail.com>
2014-05-21 14:54:03 +0900

    conditionDescriptionJSONTransformer

--- SimpleWeather/SimpleWeather/WXCondition.m
+++ SimpleWeather/SimpleWeather/WXCondition.m
@@ -84,4 +84,23 @@
     return [self dateJSONTransformer];
 }
 
++ (NSValueTransformer *)conditionDescriptionJSONTransformer
+{
+    return [MTLValueTransformer reversibleTransformerWithForwardBlock:^(NSArray *values) {
+        return [values firstObject];
+    } reverseBlock:^(NSString *str) {
+        return @[str];
+    }];
+}
+
++ (NSValueTransformer *)conditionJSONTransformer
+{
+    return [self conditionDescriptionJSONTransformer];
+}
+
++ (NSValueTransformer *)iconJSONTransformer
+{
+    return [self conditionDescriptionJSONTransformer];
+}
+
 @end
