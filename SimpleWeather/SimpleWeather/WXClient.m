adb185a0984d41a351a2d26d80e4ec88a5c701e9
chocoby <chocoby@gmail.com>
2014-05-23 10:14:31 +0900

    createSignal

--- SimpleWeather/SimpleWeather/WXClient.m
+++ SimpleWeather/SimpleWeather/WXClient.m
@@ -8,6 +8,44 @@
 
 #import "WXClient.h"
 
+#import "WXCondition.h"
+#import "WXDailyForecast.h"
+
+@interface WXClient ()
+
+@property (nonatomic, strong) NSURLSession *session;
+
+@end
+
 @implementation WXClient
+
+- (id)init
+{
+    if (self = [super init]) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config];
+    }
+
+    return self;
+}
+
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url
+{
+    NSLog(@"Fetching: %@", url.absoluteString);
+
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            // TODO: Handle retriving data
+        }];
+
+        [dataTask resume];
+
+        return [RACDisposable disposableWithBlock:^{
+            [dataTask cancel];
+        }];
+    }] doError:^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
 
 @end
