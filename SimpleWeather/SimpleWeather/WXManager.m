d18ea4cb6efb6cd47f134495e55b6fc87d0fede3
chocoby <chocoby@gmail.com>
2014-05-25 17:25:19 +0900

    Imports, Private interfaces

--- SimpleWeather/SimpleWeather/WXManager.m
+++ SimpleWeather/SimpleWeather/WXManager.m
@@ -8,6 +8,22 @@
 
 #import "WXManager.h"
 
+#import "WXClient.h"
+#import <TSMessages/TSMessage.h>
+
+@interface WXManager()
+
+@property (nonatomic, strong, readwrite) WXCondition *currentCondition;
+@property (nonatomic, strong, readwrite) CLLocation *currentLocation;
+@property (nonatomic, strong, readwrite) NSArray *hourlyForecast;
+@property (nonatomic, strong, readwrite) NSArray *dailyForecast;
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, assign) BOOL isFirstUpdate;
+@property (nonatomic, strong) WXClient *client;
+
+@end
+
 @implementation WXManager
 
 @end
